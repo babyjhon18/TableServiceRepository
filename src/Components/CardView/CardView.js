4f15eb30e9fbd8b1c18d1c297effd45e9bec214e
babyjhon18 <lolkill278@gmail.com>
2022-11-17 01:32:05 +0300

    add cards and table items

--- src/Components/CardView/CardView.js
+++ src/Components/CardView/CardView.js
@@ -1,7 +1,22 @@
+import Card from '../Card/Card'
+
 function CardView(){
     return(
         <div>
-            
+            <div>
+                <Card></Card>
+            </div>
+            <div>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+            </div>
         </div>
     );
 }
