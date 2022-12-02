c656d1e06c2bd9be2f673bc5e3f1844cb98d5231
lolkill278@gmail.com <lolkill278@gmail.com>
2022-12-02 10:02:00 +0300

    Remove useless useEffect

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -3,29 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import TableView from '../TableView/TableView';
 import CardView from '../CardView/CardView';    
 import StartMenu from '../StartMenu/StartMenu';    
-import {LOG_IN, LOGIN} from '../../Store/Constants';
 
 function MainPage(){
 
-    const dispatch = useDispatch();
     const login = useSelector(state => state.loginReduser);
-    
-    useEffect(() => {
-        if(login.isChit === undefined){
-            fetch("http://" + window.SERVER_IP + LOGIN + login.terminalID)
-            .then(response => response.json())
-            .then(result => {
-                if(result !== []){
-                    dispatch({type: LOG_IN, payload: { 
-                        isChit: result.ischit, terminalDescription: result.description, terminalID: result.id, serviceIP: window.SERVER_IP
-                    }})
-                }
-            });
-        }
-    }, [])
 
-    if((login.serviceIP !== undefined && login.serviceIP !== null) && 
-    (login.terminalID !== undefined && login.terminalID !== null)){
+    if((login.serviceIP !== "") && 
+    (login.terminalID !== 0)){
         if(login.isChit !== undefined){
             if(login.isChit){
                 return <CardView></CardView>
