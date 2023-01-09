7419faced7c71ba6ff8047a04cc8cb0a18934de2
lolkill278@gmail.com <lolkill278@gmail.com>
2023-01-09 09:23:38 +0300

    Column fixes

--- src/Components/RowElement/RowElement.js
+++ src/Components/RowElement/RowElement.js
@@ -25,28 +25,25 @@ function RowElement(props){
     if(props.NotHeader){
         return(<div>
                 <div className="mainRow row col-lg-12">
-                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
-                        {props.tableItem.pid}
+                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "80px", maxWidth: "150px"}}>
+                        {props.tableItem.received}
                     </div>
-                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
+                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "60px", maxWidth: "150px"}}>
                         {props.tableItem.tableno}
                     </div>
                     <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
                         {props.tableItem.docnumber}
                     </div>
-                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
-                        {props.tableItem.received}
-                    </div>
-                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
+                    <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "50px", maxWidth: "150px"}}>
                         {props.tableItem.qty}
                     </div>
-                    <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3" style={{minWidth: "300px"}}>
+                    <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3" style={{minWidth: "150px", maxWidth: "400px"}}>
                         {props.tableItem.name}
                     </div>
-                    <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3" style={{minWidth: "300px"}}>
+                    <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3" style={{minWidth: "150px", maxWidth: "550px"}}>
                         {props.tableItem.modifiers}
                     </div>
-                    <div className="col" style={{minWidth: "100px"}}></div>
+                    <div className="col-1" style={{minWidth: "200px"}}></div>
                 </div>
             </div>
         );
@@ -54,10 +51,10 @@ function RowElement(props){
     else{
         return(
             <div className="mainRow row col-lg-12">
-                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
-                    {props.tableItem.pid}
+                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "80px", maxWidth: "150px"}}>
+                    {props.tableItem.received}
                 </div>
-                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
+                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "60px", maxWidth: "150px"}}>
                     {props.tableItem.tableno}
                 </div>
                 <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
@@ -71,23 +68,20 @@ function RowElement(props){
                         <div>{props.tableItem.docnumber}</div>
                     }
                 </div>
-                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
-                    {props.tableItem.received}
-                </div>
-                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "100px"}}>
+                <div className="col-md-1 col-sm-1 col-lg-1 col-xs-1 col-xl-1" style={{minWidth: "50px", maxWidth: "150px"}}>
                     {props.tableItem.qty}
                 </div>
-                <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3 itemName" style={{minWidth: "300px"}}>
+                <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3 itemName" style={{minWidth: "150px", maxWidth: "400px"}}>
                     {props.tableItem.name}
                     <br></br>
                     <div className='description'>{props.tableItem.description}</div>
                 </div>
-                <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3" style={{minWidth: "300px"}}>
+                <div className="col-md-3 col-sm-3 col-lg-3 col-xs-3 col-xl-3" style={{minWidth: "150px", maxWidth: "550px"}}>
                     {props.tableItem.modifiers && props.tableItem.modifiers.map((item, index) => (
                         <TableModifier key={index} modifier={item}></TableModifier> 
                     ))}
                 </div>
-                <div className="col bump" style={{minWidth: "100px"}}>
+                <div className="col-1 bump" style={{minWidth: "200px"}}>
                     <button onClick={(event) => bumpCard(event, props.tableItem)} value={props.tableItem}>Bump</button>
                 </div>    
             </div>
